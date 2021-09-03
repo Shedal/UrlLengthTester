da0e07070fc8e59506d76d3d22b2c44e026133a4
Dmytro Shevchenko <dmytro_shevchenko3@epam.com>
2021-09-04 01:57:02 +0200

    Added an ability to execute requests via "Enter".

--- ClientApp/src/components/UrlLengthTester.js
+++ ClientApp/src/components/UrlLengthTester.js
@@ -11,13 +11,18 @@ export class UrlLengthTester extends Component {
 
   render() {
     let contents =
-      (!!this.state.returnedData && <div><h4>Server-side view:</h4><div>{this.state.returnedData}</div></div>) ||
-      (!!this.state.returnedError && <div><h4>Error:</h4><div>{this.state.returnedError}</div></div>);
+      (!!this.state.returnedData && <div><h4>Server-side view:</h4>
+        <div>{this.state.returnedData}</div>
+      </div>) ||
+      (!!this.state.returnedError && <div><h4>Error:</h4>
+        <div>{this.state.returnedError}</div>
+      </div>);
 
     return (
       <div className="w-50 justify-content-center align-self-center text-white text-center align-middle">
         <div className="pb-3">
-          <Input className="text-center" placeholder="Characters to send" onChange={this.handleInputChange}/>
+          <Input className="text-center" placeholder="Characters to send" onChange={this.handleInputChange}
+                 onKeyDown={this.handleInputKeyDown}/>
         </div>
         <button className="btn btn-block btn-outline-primary px-4 me-sm-3 fw-bold"
                 onClick={this.sendApiRequest}>Send
@@ -36,9 +41,9 @@ export class UrlLengthTester extends Component {
 
   sendApiRequest = async () => {
     this.setState({ returnedData: "", returnedError: "" });
-    
+
     const url = `LengthTest?p=${'a'.repeat(this.state.charsToSend)}`;
-    
+
     fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -49,5 +54,11 @@ export class UrlLengthTester extends Component {
       .then(response => response.text())
       .then(data => this.setState({ returnedData: data }))
       .catch(error => this.setState({ returnedError: error }));
+  }
+
+  handleInputKeyDown = async (event) => {
+    if (event.key === 'Enter') {
+      await this.sendApiRequest();
+    }
   }
 }
